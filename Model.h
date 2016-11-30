ad86168f95edb7cd364f164fda87411d94e06789
Tracy Brown <tracybrownjr@gmail.com>
2016-11-30 00:00:46 -0500

    Now using Assimp to load Crysis 2 model

---
+++ Model.h
@@ -0,0 +1,177 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "Mesh.h"
+
+#include <GL/glew.h> // Contains all the necessery OpenGL includes
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <iostream>
+
+GLint TextureFromFile(const char* path, const std::string& directory);
+
+class Model 
+{
+public:
+    Model(const std::string& path) 
+    {
+        this->loadModel(path);
+    }
+
+    void Draw(const Shader& shader)
+    {
+        for(auto i = 0; i < this->meshes.size(); ++i)
+        {
+            this->meshes[i].Draw(shader);
+        }
+    }
+
+private:
+    void loadModel(const std::string& path)
+    {
+        Assimp::Importer importer;
+        const aiScene* pScene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+        if(!pScene || pScene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || !pScene->mRootNode)
+        {
+            std::cout << "ERROR::ASSIMP::" << importer.GetErrorString() << std::endl;
+            return;
+        }
+
+        this->directory = path.substr(0, path.find_last_of('/'));
+        this->processNode(pScene->mRootNode, pScene);
+    }
+
+    void processNode(aiNode* pNode, const aiScene* pScene)
+    {
+        for(auto i = 0; i < pNode->mNumMeshes; ++i)
+        {
+            aiMesh* pMesh = pScene->mMeshes[ pNode->mMeshes[i] ];
+            this->meshes.push_back( this->processMesh(pMesh, pScene) );
+        }
+
+        for(auto i = 0; i < pNode->mNumChildren; ++i)
+        {
+            this->processNode(pNode->mChildren[i], pScene);
+        }
+    }
+
+    Mesh processMesh(aiMesh* pMesh, const aiScene* pScene)
+    {
+        std::vector<Vertex> vertices;
+        std::vector<GLuint> indices;
+        std::vector<Texture> textures;
+
+        for(auto i = 0; i < pMesh->mNumVertices; ++i)
+        {
+            Vertex vertex;
+
+            // Process vertices
+            vertex.position.x = pMesh->mVertices[i].x;
+            vertex.position.y = pMesh->mVertices[i].y;
+            vertex.position.z = pMesh->mVertices[i].z;
+
+            vertex.normal.x = pMesh->mNormals[i].x;
+            vertex.normal.y = pMesh->mNormals[i].y;
+            vertex.normal.z = pMesh->mNormals[i].z;
+
+            if(pMesh->mTextureCoords[0])
+            {
+                vertex.texCoord.x = pMesh->mTextureCoords[0][i].x;
+                vertex.texCoord.y = pMesh->mTextureCoords[0][i].y;
+            }
+            else
+            {
+                vertex.texCoord = glm::vec2(0.0f, 0.0f);
+            }
+            
+            vertices.push_back(vertex);
+        }
+
+        // Process indices
+        for(auto i = 0; i < pMesh->mNumFaces; ++i)
+        {
+            aiFace face = pMesh->mFaces[i];
+            for(auto j = 0; j < face.mNumIndices; ++j)
+            {
+                indices.push_back(face.mIndices[j]);
+            }
+        }
+        
+        if(pMesh->mMaterialIndex >= 0)
+        {
+            aiMaterial* pMaterial = pScene->mMaterials[pMesh->mMaterialIndex];
+            std::vector<Texture> diffuseMaps = this->loadMaterialTextures(pMaterial, 
+                                                aiTextureType_DIFFUSE, "texture_diffuse");
+            textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+            std::vector<Texture> specularMaps = this->loadMaterialTextures(pMaterial, 
+                                                aiTextureType_SPECULAR, "texture_specular");
+            textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+        }
+
+        return Mesh(vertices, indices, textures);
+    }
+
+    std::vector<Texture> loadMaterialTextures(aiMaterial* pMat, const aiTextureType& textureType, const std::string& typeName)
+    {
+        std::vector<Texture> textures;
+        for(auto i = 0; i < pMat->GetTextureCount(textureType); ++i)
+        {
+            aiString path;
+            pMat->GetTexture(textureType, i, &path);
+            GLboolean skip = false;
+            for(GLuint j = 0; j < textures_loaded.size(); j++)
+            {
+                if(textures_loaded[j].path == path)
+                {
+                    textures.push_back(textures_loaded[j]);
+                    skip = true; 
+                    break;
+                }
+            }
+            if(!skip)
+            {   // If texture hasn't been loaded already, load it
+                Texture texture;
+                texture.id = TextureFromFile(path.C_Str(), this->directory);
+                texture.type = typeName;
+                texture.path = path;
+                textures.push_back(texture);
+                this->textures_loaded.push_back(texture);  // Add to loaded textures
+            }
+        }
+        return textures;
+    }
+
+    std::vector<Mesh> meshes;
+    std::vector<Texture> textures_loaded;
+    std::string directory;
+};
+
+GLint TextureFromFile(const char* path, const std::string& directory)
+{
+     //Generate texture ID and load texture data 
+    std::string filename = directory + '/' + std::string(path);
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    int width, height, channels;
+    unsigned char* image = stbi_load(filename.c_str(), &width, &height, &channels, 0);
+    // Assign texture to ID
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);	
+
+    // Parameters
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    stbi_image_free(image);
+    return textureID;
+}
+
+#endif // MODEL_H
--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 
 #include "Shader.h"
 #include "Camera.h"
+#include "Model.h"
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
@@ -77,145 +78,10 @@ int main(int argc, char** argv)
 
     glViewport(0, 0, WIDTH, HEIGHT);
 
-	Shader basicShader("resources/shaders/basic.vert", "resources/shaders/basic.frag");
-	Shader lightShader("resources/shaders/light.vert", "resources/shaders/light.frag");
-    
-    GLfloat vertices[] = {
-        // Positions           // Normals           // Texture Coords
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
-
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
-
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
-
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
-    };
-
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f),
-        glm::vec3( 2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f,  2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
-    
-
-    std::vector<glm::vec3> pointLightPositions = {
-        glm::vec3( 0.7f,  0.2f,  2.0f),
-        glm::vec3( 2.3f, -3.3f, -4.0f),
-        glm::vec3(-4.0f,  2.0f, -12.0f),
-        glm::vec3( 0.0f,  0.0f, -3.0f)
-    };
-
-	GLuint VAO;
-	GLuint VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-        
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	GLuint sizeOfVertex = 8 * sizeof(GLfloat);
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeOfVertex, (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-	// Normal attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeOfVertex, (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-    // TexCoord attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeOfVertex, (GLvoid*)(6 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(2);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(0);
-
-	GLuint lightVAO;
-	glGenVertexArrays(1, &lightVAO);
-
-	glBindVertexArray(lightVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeOfVertex, (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-	glBindVertexArray(0);
-
-    GLuint diffuseMap;
-    glGenTextures(1, &diffuseMap);
-    glBindTexture(GL_TEXTURE_2D, diffuseMap);
-
-    // Set the texture wrapping settings
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // Set the texture filtering settings
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // Load image, create texture and generate mipmaps
-    int width, height, channels;
-    unsigned char* image = stbi_load("resources/images/container2.png", &width, &height, &channels, 0);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, 0);
-    stbi_image_free(image);
-
-    GLuint specularMap;
-    glGenTextures(1, &specularMap);
-    glBindTexture(GL_TEXTURE_2D, specularMap);
-
-    // Set the texture wrapping settings
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // Set the texture filtering settings
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // Load image, create texture and generate mipmaps
-    image = stbi_load("resources/images/container2_specular.png", &width, &height, &channels, 0);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, 0);
-    stbi_image_free(image);
-
     glEnable(GL_DEPTH_TEST);
+
+    Shader shader("resources/shaders/model_loading.vert", "resources/shaders/model_loading.frag");
+    Model nanosuitModel("resources/nanosuit2.obj");
 
     while(!glfwWindowShouldClose(window))
     {
@@ -229,124 +95,22 @@ int main(int argc, char** argv)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		basicShader.Use();
+        shader.Use();
+        // Transformation matrices
+        glm::mat4 projection = glm::perspective(camera.Zoom, (float)WIDTH/(float)HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
+        // Draw the loaded model
         glm::mat4 model;
-        glm::mat4 projection;
-        projection = glm::perspective(glm::radians(camera.Zoom), WIDTH / static_cast<GLfloat>(HEIGHT), 0.1f, 100.0f);
-
-        GLuint modelLoc = glGetUniformLocation(basicShader.Program, "model");
-		GLuint viewLoc = glGetUniformLocation(basicShader.Program, "view");
-        GLuint projectionLoc = glGetUniformLocation(basicShader.Program, "projection");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(camera.GetViewMatrix()));
-		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-		GLint viewPosLoc = glGetUniformLocation(basicShader.Program, "viewPos");
-
-		glUniform3f(viewPosLoc, camera.Position.x, camera.Position.y, camera.Position.z);	
-
-        glm::vec3 lightColor(1.0f);
-        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
-        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
-        
-        GLint dirLightDirLoc = glGetUniformLocation(basicShader.Program, "dirLight.direction");
-        GLint dirLightAmbientLoc = glGetUniformLocation(basicShader.Program, "dirLight.ambient");
-        GLint dirLightDiffuseLoc = glGetUniformLocation(basicShader.Program, "dirLight.diffuse");
-        GLint dirLightSpecularLoc = glGetUniformLocation(basicShader.Program, "dirLight.specular");
-        glUniform3f(dirLightDirLoc, -0.2f, -1.0f, -0.3f);
-        glUniform3f(dirLightAmbientLoc, ambientColor.x, ambientColor.y, ambientColor.z);
-        glUniform3f(dirLightDiffuseLoc, diffuseColor.x, diffuseColor.y, diffuseColor.z);
-        glUniform3f(dirLightSpecularLoc, lightColor.x, lightColor.y, lightColor.z);
-
-        for (auto i = 0; i < pointLightPositions.size(); ++i) {
-            std::string pointLightName = "pointLights[" + std::to_string(i) + "]";
-            std::string positionName = pointLightName + ".position";
-            std::string ambientName = pointLightName + ".ambient";
-            std::string diffuseName = pointLightName + ".diffuse";
-            std::string specularName = pointLightName + ".specular";
-            std::string constantName = pointLightName + ".constant";
-            std::string linearName = pointLightName + ".linear";
-            std::string quadraticName = pointLightName + ".quadratic";
-
-            glUniform3f(
-                glGetUniformLocation(basicShader.Program, positionName.c_str()), 
-                pointLightPositions[i].x, 
-                pointLightPositions[i].y, 
-                pointLightPositions[i].z
-            );
-            glUniform3f(
-                glGetUniformLocation(basicShader.Program, ambientName.c_str()), 
-                ambientColor.x, 
-                ambientColor.y, 
-                ambientColor.z
-            );
-            glUniform3f(
-                glGetUniformLocation(basicShader.Program, diffuseName.c_str()), 
-                diffuseColor.x, 
-                diffuseColor.y, 
-                diffuseColor.z
-            );
-            glUniform3f(
-                glGetUniformLocation(basicShader.Program, specularName.c_str()), 
-                lightColor.x, 
-                lightColor.y, 
-                lightColor.z
-            );
-
-            glUniform1f(glGetUniformLocation(basicShader.Program, constantName.c_str()), 1.0f);
-            glUniform1f(glGetUniformLocation(basicShader.Program, linearName.c_str()), 0.09f);
-            glUniform1f(glGetUniformLocation(basicShader.Program, quadraticName.c_str()), 0.032f);
-        }
-
-        glUniform1i(glGetUniformLocation(basicShader.Program, "material.diffuse"), 0);
-        glUniform1i(glGetUniformLocation(basicShader.Program, "material.specular"), 1);
-        glUniform1f(glGetUniformLocation(basicShader.Program, "material.shininess"), 64.0f);
-
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, diffuseMap);
-
-        glActiveTexture(GL_TEXTURE0 + 1);
-        glBindTexture(GL_TEXTURE_2D, specularMap);
-
-        glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-        for(GLuint i = 0; i < 10; i++)
-        {
-           model = glm::mat4();
-           model = glm::translate(model, cubePositions[i]);
-           GLfloat angle = 20.0f * i; 
-           model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
-           glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-           glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-        glBindVertexArray(0);
-
-		lightShader.Use();
-
-        viewLoc = glGetUniformLocation(lightShader.Program, "view");
-        projectionLoc = glGetUniformLocation(lightShader.Program, "projection");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(camera.GetViewMatrix()));
-        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-        
-        for (auto i = 0; i < pointLightPositions.size(); ++i) {
-            model = glm::mat4();
-            model = glm::translate(model, pointLightPositions[i]);
-            model = glm::scale(model, glm::vec3(0.2f));
-
-            modelLoc = glGetUniformLocation(lightShader.Program, "model");
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-            glBindVertexArray(lightVAO);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-            glBindVertexArray(0);
-        }
+        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// It's a bit too big for our scene, so scale it down
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        nanosuitModel.Draw(shader);
 
         glfwSwapBuffers(window);
     }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;
