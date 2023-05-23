6e940aba93ff9c0f3df2707422c8b313648b81fa
Tracy Brown Jr <tracybrownjr@gmail.com>
2023-05-22 23:08:12 -0500

    Formatting main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -40,111 +40,111 @@ float yRotationSpeed = glm::pi<float>() / HEIGHT;
 bool keys[1024];
 
 struct PointLight {
-    glm::vec3 position;
+  glm::vec3 position;
 
-    glm::vec3 ambient;
-    glm::vec3 diffuse;
-    glm::vec3 specular;
+  glm::vec3 ambient;
+  glm::vec3 diffuse;
+  glm::vec3 specular;
 
-    float constant;
-    float linear;
-    float quadratic;
+  float constant;
+  float linear;
+  float quadratic;
 };
 
 int main(int argc, char** argv)
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
-    if (window == nullptr)
-    {
-        std::printf("Failed to create GLFW window\n" );
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    glfwSetKeyCallback(window, key_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
-
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
-    {
-        std::printf("Failed to initialize OpenGL context\n" );
-        return -1;
-    }
-
-    glViewport(0, 0, WIDTH, HEIGHT);
-
-    glEnable(GL_DEPTH_TEST);
-
-    Shader shader {};
-    shader.fromFile("resources/shaders/model_loading.vert", "resources/shaders/model_loading.frag");
-
-    Model nanosuitModel("resources/nanosuit.obj");
-
-    // Setting up ImGUI
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 460 core");
-
-    while(!glfwWindowShouldClose(window))
-    {
-		GLfloat currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-
-        glfwPollEvents();
-		do_movement(deltaTime);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        shader.Use();
-        // Transformation matrices
-        glm::mat4 projection = glm::perspective(90.0f, (float)WIDTH/(float)HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = convert_to_view_matrix(camera);
-        glm::mat4 model(1.0f);
-        // Translate it down a bit so it's at the center of the scene
-        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f));
-        // It's a bit too big for our scene, so scale it down
-        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "mvp"), 1, GL_FALSE, glm::value_ptr(projection*view*model));
-        nanosuitModel.Draw(shader);
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-        {
-            ImGui::ShowDemoWindow();
-        }
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
-    }
-
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+  if (window == nullptr)
+  {
+    std::printf("Failed to create GLFW window\n" );
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  glfwSetKeyCallback(window, key_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
+  glfwSetScrollCallback(window, scroll_callback);
+
+  if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+  {
+    std::printf("Failed to initialize OpenGL context\n" );
+    return -1;
+  }
+
+  glViewport(0, 0, WIDTH, HEIGHT);
+
+  glEnable(GL_DEPTH_TEST);
+
+  Shader shader {};
+  shader.fromFile("resources/shaders/model_loading.vert", "resources/shaders/model_loading.frag");
+
+  Model nanosuitModel("resources/nanosuit.obj");
+
+  // Setting up ImGUI
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 460 core");
+
+  while(!glfwWindowShouldClose(window))
+  {
+    GLfloat currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
+    glfwPollEvents();
+    do_movement(deltaTime);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    shader.Use();
+    // Transformation matrices
+    glm::mat4 projection = glm::perspective(90.0f, (float)WIDTH/(float)HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = convert_to_view_matrix(camera);
+    glm::mat4 model(1.0f);
+    // Translate it down a bit so it's at the center of the scene
+    model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f));
+    // It's a bit too big for our scene, so scale it down
+    model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
+    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "mvp"), 1, GL_FALSE, glm::value_ptr(projection*view*model));
+    nanosuitModel.Draw(shader);
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+    {
+      ImGui::ShowDemoWindow();
+    }
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+  }
+
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwTerminate();
+  return 0;
 }
 
 void do_movement(GLfloat deltaTime)
@@ -153,36 +153,36 @@ void do_movement(GLfloat deltaTime)
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-	if (action == GLFW_PRESS)
-		keys[key] = true;
-	if (action == GLFW_RELEASE)
-		keys[key] = false;
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
+  if (action == GLFW_PRESS)
+    keys[key] = true;
+  if (action == GLFW_RELEASE)
+    keys[key] = false;
 }
 
 bool firstMouse = true;
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
-	if (firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
+  if (firstMouse)
+  {
+    lastX = xpos;
+    lastY = ypos;
+    firstMouse = false;
+  }
 
-	float deltaX = xpos - lastX;
-    // Invert Y
-	float deltaY = lastY - ypos;
+  float deltaX = xpos - lastX;
+  // Invert Y
+  float deltaY = lastY - ypos;
 
-    camera.swivel_radians += deltaX * xRotationSpeed;
-    camera.pitch_radians += deltaY * yRotationSpeed;
+  camera.swivel_radians += deltaX * xRotationSpeed;
+  camera.pitch_radians += deltaY * yRotationSpeed;
 
-	lastX = xpos;
-	lastY = ypos;
+  lastX = xpos;
+  lastY = ypos;
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-    camera.distance += yoffset*0.25;
+  camera.distance += yoffset*0.25;
 }
